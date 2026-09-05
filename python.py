2f642bec439fbc856d78fd98a4e09b700b53e2b3
Padel20x10 <padel20x10app@gmail.com>
2026-09-05 19:54:45 -0300

    agregar number_4 a la suma

--- python.py
+++ python.py
@@ -1,5 +1,6 @@
 number_1 = 10
 number_2 = 20
 number_3 = 30
-c = number_1 + number_2 + number_3
-print("la suma de lso tres numero es:", c)
+number_4 = 40
+c = number_1 + number_2 + number_3 + number_4
+print("la suma de lso cuatro numero es:", c)

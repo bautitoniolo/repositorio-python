86e763da97c9c1d3ff287fb417faa0b6233d11f5
Padel20x10 <padel20x10app@gmail.com>
2026-09-06 10:04:29 -0300

    nuevas lineas

--- python.py
+++ python.py
@@ -10,3 +10,18 @@ number_4 = 40
 number_5 = 50
 d = number_4 + number_5
 print(la suma de lso dos numero es:, d)
+number_1 = int(input(ingrese un numero:))
+number_2 = int(input(ingrese el segundo numero: ))
+number_3 = int(input( ingrese el tercer numero: ))
+if number_1 == number_2 and number_3:
+     print(los tres lados son iguales)
+else: 
+    number_1 != number_2 == number_3
+    print(dos lados son iguales y uno no )
+if number_1 == number_2 != number_3:
+    print(dos lados son iguales y uno no )
+if number_1 == number_3 != number_2:
+    print(dos lados son iguales y uno no )
+else:
+    number_1 != number_2 != number_3
+    print(los tres lados son diferentes)

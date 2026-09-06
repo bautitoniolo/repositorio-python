b4ebff90aa53753cb6a5daea3205f3a537784a86
toniolo-bautista <juanbautistatoniolo@gmail.com>
2026-09-06 09:53:26 -0300

    Update python.py

--- python.py
+++ python.py
@@ -4,8 +4,8 @@ number_3 = 30
 number_4 = 40
 c = number_1 + number_2 + number_3 + number_4
 print("la suma de lso cuatro numero es:", c)
-hola
-print(d)
+
+
 number_4 = 40 
 number_5 = 50
 d = number_4 + number_5
